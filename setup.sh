79e2f6fff44feda0e6593e9bf3963aa5255cd163
Joshua Murphy <joshuamurphy71@gmail.com>
2018-02-17 08:26:41 -0330

    Seems to be loading configurations properly on boot now.

--- setup.sh
+++ setup.sh
@@ -13,3 +13,4 @@ xrdb -load ~/.Xresources
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 stow zsh
 
+sudo systemctl enable NetworkManager.service
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,5 +1,6 @@
 export EDITOR=nvim
 alias vim='nvim'
+alias vi='nvim'
 
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
