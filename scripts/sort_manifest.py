c2a3e51eac1442edc2a0e006ef431d4cdcfeb0c8
Joshua Murphy <joshuamurphy71@gmail.com>
2018-03-02 18:46:52 -0330

    Updated manifest and fixed sort script

--- scripts/sort_manifest.py
+++ scripts/sort_manifest.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python
  
-# Dotfiles - sort_manifest.py w
+# Dotfiles - sort_manifest.py 
 # Author: Joshua Murphy
 # Github: https://github.com/ranguli/dotfiles
 # Sorts package manifests alphabetically. Should be run before every time
 # manifests are commited.
 
 import sys
-usage = "Supply filenames/paths that you want the contents sorted." 
 
-if len(sys.argv) != 1 and sys.argv[1] != '-h' and sys.argv[1] != '--h':
-    for i in range(len(sys.argv)):
-            filename = sys.argv[i]
-            manifest = []
-            file = open(filename)
-            contents = file.readlines()
-    for n, line in enumerate(contents):
-        manifest.append(line.rstrip())
+for i in range(1, len(sys.argv)):
+    filename = sys.argv[i]
+    file = open(filename)
+    contents = file.readlines()
     
-    manifest.sort()
-
+    contents.sort()
+    print(len(contents))
     file = open(filename, 'w')
-    for i in range (0, len(manifest)):
-        file.write(manifest[i] + '\n')
 
-else: 
-    print(usage)
-
+    for x in range(len(contents)):
+        line = (contents[x])
+        print(line)
+        file.write(line)
