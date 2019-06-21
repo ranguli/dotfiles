68960dea0b695234b5e4dd599d110da602d3c540
Jack Arthur Harrhy <me@jackharrhy.com>
2019-06-21 16:54:39 -0230

    remove bad

--- scripts/xcape.sh
+++
@@ -1,8 +0,0 @@
-# Thanks to this brilliant article, we can map Caps Lock to generate Escape 
-# When pressed on it's own, but if it's held in a combination with another 
-# key, it's Control. 
-
-# http://tiborsimko.org/capslock-escape-control.html
-
-setxkbmap -option ctrl:nocaps
-xcape -e 'Control_L=Escape'
