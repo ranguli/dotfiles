14536ea57ef670de7f69b82d9b96ab476437a4a8
ranguli <hello@joshmurphy.ca>
2019-06-21 21:16:35 -0400

    Updated bash, vim, xinit, etc

--- scripts/install_shell.sh
+++
@@ -1,5 +0,0 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-wget https://downloads.nordcdn.com/configs/archives/servers/ovpn.zip
-#xrdb -load ~/.Xresources 
-
