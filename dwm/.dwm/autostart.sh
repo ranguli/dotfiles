1116b8d3370b98d2a09191f56137ec876a2d3423
ranguli <hello@joshmurphy.ca>
2019-06-24 17:41:29 -0400

    Added dwm autostart script

---
+++ dwm/.dwm/autostart.sh
@@ -0,0 +1,37 @@
+xrdb -load ~/.Xresources
+
+function eth_info {
+    eth_nic=enp0s31f6
+    eth_ip=$(ip addr | grep $eth_nic | grep inet | awk '{print $2}')
+    if [[ $eth_ip ]]
+    then
+        echo "$eth_nic: $eth_ip |"
+    fi 
+}
+
+function wlan_info {
+    wlan_nic=wlp61s0
+    ssid=$(iw dev | grep ssid | sed 's/ssid//g' | tr -d ' \t')
+    ssid_strength=$(cat /proc/net/wireless | awk 'NR==3 {print $3}' | tr -d '.')%
+    wlan_ip=$(ip addr | grep $wlan_nic | grep inet | awk '{print $2}')
+    echo "$wlan_nic: $ssid $ssid_strength $wlan_ip |"
+}
+
+
+function get_backlight {
+    backlight=$(xbacklight -get | awk '{print int($1)}')
+    echo "Backlight: $backlight% |"
+	
+}
+
+while : 
+do
+    date="$(date) |"
+    wlan=$(wlan_info)
+    eth=$(eth_info)
+    backlight=$(get_backlight)
+    battery="Battery: $(cat /sys/class/power_supply/BAT0/capacity)% |"
+    xsetroot -name  "$date $wlan $eth $battery $backlight" 
+    sleep 0.5 
+done
+
