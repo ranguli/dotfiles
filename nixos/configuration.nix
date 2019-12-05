a42d6663dabf126e07e21b24d5709c912f3ad805
ranguli <hello@joshmurphy.ca>
2019-12-05 18:25:26 -0330

    Restored NixOS config that got replaced by the default one

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -14,16 +14,16 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "eurydices"; # Define your hostname.
+  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
   networking.useDHCP = false;
   networking.interfaces.enp0s31f6.useDHCP = true;
-  networking.interfaces.vboxnet0.useDHCP = true;
   networking.interfaces.wlp61s0.useDHCP = true;
+  networking.interfaces.enp0s20f0u2.useDHCP = true;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
@@ -37,13 +37,62 @@
   # };
 
   # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
+  time.timeZone = "America/St_Johns";
+
+  # Set zsh as shell 
+  programs.zsh.enable = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   wget vim
-  # ];
+  
+  environment.systemPackages = with pkgs; [
+     # dev tools
+     vim
+     python3
+     virtualbox
+     vagrant
+     vscode
+    
+     # games 
+     minecraft
+     xonotic
+
+     # apps
+     firefox
+     slack
+     discord
+     spotify
+     steam
+     alacritty
+     weechat
+     gimp
+    
+     # cli tools 
+     zsh
+     stow
+     oh-my-zsh
+     wget
+     fd
+     bat
+     jq
+     ripgrep-all
+     lsd
+     xclip
+     _1password
+     ansible
+     git
+     ranger
+     tmux
+
+     # misc.
+     gnome3.gnome-tweaks
+     gnome3.gnome-tweak-tool
+     jre
+  ];
+
+  nixpkgs.config = {
+    allowUnfree = true; # I couldn't get it to only allow specific packages :(
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -62,29 +111,39 @@
   # networking.firewall.enable = false;
 
   # Enable CUPS to print documents.
-  # services.printing.enable = true;
+  services.printing.enable = true;
 
   # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Steam support:
+  hardware.opengl.driSupport32Bit = true;
+  hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
+  hardware.pulseaudio.support32Bit = true;
+
 
   # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-  # services.xserver.layout = "us";
+  services.xserver.enable = true;
+  services.xserver.layout = "us";
   # services.xserver.xkbOptions = "eurosign:e";
 
-  # Enable touchpad support.
-  # services.xserver.libinput.enable = true;
+  # Enable Gnome 3
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.wayland = false;
+  services.xserver.desktopManager.gnome3.enable = true;
+  #services.xserver.windowManager.i3.enable = true;
 
-  # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.desktopManager.plasma5.enable = true;
+  # Enable touchpad support.
+  services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.jane = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  # };
+  users.users.joshua = {
+    isNormalUser = true;
+    home = "/home/joshua";
+    shell = pkgs.zsh;
+    extraGroups = [ "wheel" "networkmanager" "docker" ]; # Enable ‘sudo’ for the user.
+  };
 
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
